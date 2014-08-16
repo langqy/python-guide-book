cd67b9f713561cfca5cfbf94f73ccd3b0ae4e518
a358003542 <a358003542@gmail.com>
2014-08-17 00:31:49 +0800

    20140817

--- codeexport/code:5-8/code:5-8.py
+++ codeexport/code:5-8/code:5-8.py
@@ -1,4 +1,17 @@
 
-f=lambda x,y,z:x+y+z
-print(f(1,2,3))
+lst = [[1,2,[3]],[4,[5,[[[[10],11]]]],(1,2,3)],[{'a','b','c'},8,9]]
+
+def is_list(thing):
+    return isinstance(thing, list)
+
+def flatten(iter):
+    templst = []
+    for x in iter:
+        if not is_list(x):
+            templst.append(x)
+        else:
+            templst += flatten(x)
+    return templst
+
+print(flatten(lst))
 
